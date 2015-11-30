4a98ff672b649c05276ae2e2d6e6dac6606cbbb3
cenenh <gseunwo@gmail.com>
2015-11-30 21:40:59 +0900

    modify auth_config

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '698775146890408',
         'clientSecret'  : '5326b5b75fde359eb882c0263f03be58',
-        'callbackURL'   : 'http://54.65.85.60:8080/auth/facebook/callback'
+        'callbackURL'   : 'http://54.65.85.60:8080/auth/facebook_web/callback'
     },
     'googleAuth' : {
         'clientID'      : '985262129544-3hvvbbgsal02mdotgstvnls355fd8nir.apps.googleusercontent.com',

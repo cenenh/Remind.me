dd7ae8173416690abb70af7c815bb9fd87aecd4f
cenenh <gseunwo@gmail.com>
2015-12-01 08:15:34 +0000

    modify addUser() for sign-up using facebook

--- app/Controller/User.js
+++ app/Controller/User.js
@@ -1,4 +1,5 @@
 var User = require('../Model/User');
+var _ = require('underscore');
 var async = require('async');
 
 module.exports.getAll = function(req, res){
@@ -17,21 +18,29 @@ module.exports.addUser = function(req, res){
 
   console.log("request method : " + req.method);
   console.log("user.addUser() is called!");
-
+  
   var response = {};
   var newUser = new User();
-
-  newUser.name = req.body.name;
-  newUser.password = req.body.password;
-  newUser.email = req.body.email;
-  console.log(newUser);
-
+  
+  if(_.isEmpty(req.params)){ //sign-up using email
+	newUser.name = req.body.name;
+	newUser.password = req.body.password;
+	newUser.email = req.body.email;
+	console.log(newUser);
+  }
+  else{	//sign-up using Facebook or Google
+	var req_data = JSON.parse(req.params.user);
+	newUser.name = req_data.name;
+	newUser.password = req_data.password;
+	newUser.email = req_data.email;
+  }
+	
   newUser.addUser(newUser, function(error, result){
     if(error){
       response.code = 400;
       response.data = "addUser Fail";
       if(error.errno === 1062){
-        response.reason = "duplicate e-mail";
+        response.reason = "duplicate-mail";
       }
     }
     else{
@@ -42,3 +51,4 @@ module.exports.addUser = function(req, res){
   });
   delete newUser;
 };
+

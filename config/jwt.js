90564e614af44fa61ef9c8de0af7c5e800de70a8
cenenh <gseunwo@gmail.com>
2015-12-04 01:46:12 +0900

    set config & test code for jwt

---
+++ config/jwt.js
@@ -0,0 +1,6 @@
+module.exports = {
+    secret : "remind_me",
+    expires : 1440
+};
+// expiresInMinutes : 1440
+// 24 hours
--- self-check/check_jwt.js
+++ self-check/check_jwt.js
@@ -1,13 +1,18 @@
-var sec = "remind.me";
 var jwt = require('jsonwebtoken');
-var token = jwt.sign({ name: 'choieunho' }, sec);
-console.log(token);
+var jwt_config = require('../config/jwt');
+console.log(jwt_config.secret);
+var data = {
+  email : 'cenenh@naver.com',
+  name : 'Choi Eun Ho',
+  password : '1q2w3e4r'
+}
 
-jwt.verify(token, sec, function(err, decoded) {
-  if(err){
-    console.log(err);
-  }
-  else{
-    console.log(decoded.name);
-  }
+jwt.sign(data, jwt_config.secret, {}, function(done){
+  console.log(done);
+});
+
+var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImNlbmVuaEBuYXZlci5jb20iLCJuYW1lIjoiQ2hvaSBFdW4gSG8iLCJwYXNzd29yZCI6IjFxMnczZTRyIiwiaWF0IjoxNDQ5MTYwNTI3fQ.A9FBLcp0rskF0t3-1jlvKWbjbLU0noAGaIOBhoVxCK4";
+jwt.verify(token, jwt_config.secret, {}, function(err, decoded){
+  console.log(decoded);
+  console.log(decoded.email);
 });

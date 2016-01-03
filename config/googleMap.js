5efcaecd5c5c052e1a0a3f98eec2091c1c062373
cenenh <gseunwo@gmail.com>
2016-01-03 23:24:18 +0900

    add google map config

---
+++ config/googleMap.js
@@ -0,0 +1,7 @@
+module.exports = {
+  key : "AIzaSyALuZpjiForJ2p6kB5BK57uszIpokZj9tA"
+};
+/*
+https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=cruise&key=AIzaSyALuZpjiForJ2p6kB5BK57uszIpokZj9tA
+
+*/

4a74608c7c73481249149c7dabea8eb057667d3e
cenenh <gseunwo@gmail.com>
2016-01-12 21:43:10 +0900

    modify google map setting

--- config/googleMap.js
+++ config/googleMap.js
@@ -2,7 +2,7 @@ module.exports = {
   url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json',
   params : {
     location : '',
-    radius : 2500,
+    radius : 500,
     name : '',
     language : 'ko',
     key : 'AIzaSyALuZpjiForJ2p6kB5BK57uszIpokZj9tA',
